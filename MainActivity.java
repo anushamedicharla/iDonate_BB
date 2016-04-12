96fe189892892f9b09dcbe257bab7036b4d29aaf
anushamedicharla <anusha.medicharla@gmail.com>
2016-04-12 15:49:49 -0500

    Added files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -19,9 +19,10 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-    database_helper myDb;
+    com.example.loginregister.database_helper myDb;
     Button blogin;
     Button bRegister;
+    Button bHospitalNearMe;
     EditText etName, etUsername, etBloodGroup;
 
 
@@ -29,14 +30,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myDb = new database_helper(this);
+        myDb = new com.example.loginregister.database_helper(this);
 
         etUsername = (EditText) findViewById(R.id.etUsername);
         etName = (EditText) findViewById(R.id.etName);
         blogin = (Button) findViewById(R.id.blogin);
         bRegister = (Button) findViewById(R.id.bRegister);
+        bHospitalNearMe = (Button) findViewById(R.id.bHospitalsNearMe);
         blogin.setOnClickListener(this);
         bRegister.setOnClickListener(this);
+        bHospitalNearMe.setOnClickListener(this);
 
     }
 
@@ -45,12 +48,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v){
         switch (v.getId()){
             case R.id.blogin:
-                Intent intent = new Intent(this, Login.class);
+                Intent intent = new Intent(this, com.example.loginregister.Login.class);
                 startActivity(intent);
                 break;
             case R.id.bRegister:
-                Intent intent1 = new Intent(this, Register.class);
+                Intent intent1 = new Intent(this, com.example.loginregister.Register.class);
                 startActivity(intent1);
+                break;
+            case R.id.bHospitalsNearMe:
+                Intent intent2 = new Intent(this, com.example.loginregister.MapsActivity.class);
+                startActivity(intent2);
                 break;
 
 
---
+++ MapsActivity.java
@@ -0,0 +1,78 @@
+package com.example.loginregister;
+
+import android.app.Dialog;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        if(servicesOK()){
+            Toast.makeText(MapsActivity.this, "Ready to Map", Toast.LENGTH_SHORT).show();
+        }
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    public boolean servicesOK(){
+        GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
+        int isAvailable = googleAPI.isGooglePlayServicesAvailable(this);
+
+        if(isAvailable == ConnectionResult.SUCCESS)
+        {
+            return true;
+        }
+        else if(googleAPI.isUserResolvableError(isAvailable)){
+            Dialog dialog = googleAPI.getErrorDialog(this,isAvailable,ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else{
+            Toast.makeText(MapsActivity.this, "Can't Connect to mapping service", Toast.LENGTH_SHORT).show();
+        }
+
+        return false;
+    }
+
+
+}
